f6d9ed521a6668a82253d3fd4dfd72cb32d12461
GalaxyShad <vadim.kadyrin@mail.ru>
2023-03-24 17:40:13 +0500

    feat: underscores in Numeric Literals

--- SomeAsmTranslator/NumericDataParser.cs
+++ SomeAsmTranslator/NumericDataParser.cs
@@ -13,14 +13,28 @@ class NumericDataParser
     private bool IsValidBinary(string bin) => 
         Regex.IsMatch(bin, "^[01]+B$");
 
-    private string NormalizeString(string str) => str.Trim().ToUpper();
+    private string RemoveUnderscores(string source)
+    {
+        char last = source.Last();
+
+        if (last is '_')
+            throw new FormatException($"Cannot put underscores at the end of a literal -> {source}");
+
+        if (last is 'Q' or 'O' or 'D' or 'H' or 'B' && source.LastIndexOf('_') == source.Length-2 )
+            throw new FormatException($"Cannot put underscores prior to an {last} suffix -> {source}");
+
+        return source.Replace("_", string.Empty);
+    }
+       
+
+    private string NormalizeString(string str) => RemoveUnderscores(str.Trim().ToUpper());
 
     public int ParseHexadecimal(string source)
     {
         source = NormalizeString(source);
 
         if (!IsValidHex(source))
-            throw new InvalidDataException("Invalid HEX value");
+            throw new InvalidDataException($"Invalid HEX value -> {source}");
 
         return Convert.ToInt32(source[..^1], 16);
     }
@@ -30,7 +44,7 @@ class NumericDataParser
         source = NormalizeString(source);
 
         if (!IsValidDecimal(source))
-            throw new InvalidDataException("Invalid Decimal value");
+            throw new InvalidDataException($"Invalid Decimal value -> {source}");
 
         return Convert.ToInt32((source.Last() == 'D') ? source[..^1] : source, 10);
     }
@@ -40,7 +54,7 @@ class NumericDataParser
         source = NormalizeString(source);
 
         if (!IsValidOctal(source))
-            throw new InvalidDataException("Invalid Octal value");
+            throw new InvalidDataException($"Invalid Octal value -> {source}");
 
         return Convert.ToInt32(source[..^1], 8);
     }
@@ -50,12 +64,13 @@ class NumericDataParser
         source = NormalizeString(source);
 
         if (!IsValidBinary(source))
-            throw new InvalidDataException("Invalid Binary value");
+            throw new InvalidDataException($"Invalid Binary value -> {source}");
 
         return Convert.ToInt32(source[..^1], 2);
     }
 
     public byte ParseAscii(char ascii) => (byte)ascii;
+
 
     public static ushort SwapBytes(ushort x) => (ushort)((x >> 8) | (x << 8));
 }
