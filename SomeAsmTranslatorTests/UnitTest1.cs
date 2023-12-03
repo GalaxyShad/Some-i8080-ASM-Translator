fcb694430041b68b89a5ef61439d7d5a034cbd51
GalaxyShad <vadim.kadyrin@mail.ru>
2023-12-03 18:49:28 +0500

    [+] basic unit tests

--- SomeAsmTranslatorTests/UnitTest1.cs
+++ SomeAsmTranslatorTests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace SomeAsmTranslatorTests
     public class LabelTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEqu()
         {
             var asm = new Assembler(@"TEST EQU 20h
                                       CALL TEST     ");
@@ -34,7 +34,33 @@ namespace SomeAsmTranslatorTests
             var listing = asm.AssembleAll().ToList();
             listing[1].AssembledAssemblyStatement
                       .MachineCode
-                      .Should().Equal(new byte[] { 0x20 });
+                      .Should()
+                      .Equal(new byte[] { 0x20 });
         }
+    }
+
+    [TestClass]
+    public class InstructionsMachineCodeTests
+    {
+        private void TestInstruction(string sourceCode, byte[] expectedMachineCode)
+        {
+            var asm = new Assembler(sourceCode);
+
+            asm.AssembleAll()
+               .ToList()[0]
+               .AssembledAssemblyStatement
+               .MachineCode
+               .Should()
+               .Equal(expectedMachineCode);
+        }
+
+        [TestMethod]
+        public void ADD_A() => TestInstruction("ADD A", new byte[] { 0x87 });
+
+        [TestMethod]
+        public void ADD_B() => TestInstruction("ADD B", new byte[] { 0x80 });
+
+        [TestMethod]
+        public void ADI_D8() => TestInstruction("ADI 32h", new byte[] { 0xC6, 0x32 });
     }
 }
